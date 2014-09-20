2b1acf0c9233bf8ba217248684770fe7e54540b8
Yuval Oren <yuval@n3twork.com>
2014-09-19 17:47:08 -0700

    Switch to upstart, write logs to syslog

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,9 @@ maintainer_email "yuval@n3twork.com"
 license          "Apache v2"
 description      "Installs/Configures storm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 
 depends          "ark"
 depends          "java"
 depends          "maven"
-depends          "runit", "> 1.0.0"
 depends          "zeromq"

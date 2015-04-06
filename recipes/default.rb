ebd3a3ba99da44180b81f7fba4d48e27fc4cebc1
Charlie Moad <charlie.moad@geofeedia.com>
2015-04-06 11:32:58 -0400

    allowing drpc with chef client

--- recipes/default.rb
+++ recipes/default.rb
@@ -84,7 +84,7 @@ else
              nimbus_nodes.sort { |a, b| a.name <=> b.name }.first
            end
   zk_nodes = search(:node, "zookeeper_cluster_name:#{node["storm"]["zookeeper"]["cluster_name"]} AND chef_environment:#{node.chef_environment}").sort { |a, b| a.name <=> b.name }
-  raise RuntimeError, "This script will not work with chef client and drpc servers." if node['storm']['drpc']['switch']
+  # raise RuntimeError, "This script will not work with chef client and drpc servers." if node['storm']['drpc']['switch']
 end
 
 raise RuntimeError, "No zookeeper nodes found" if zk_nodes.empty?
